a7e007e941919bb2c778f90bbb3ed47e8ba23544
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-11 13:01:57 -0700

    Completed operators and expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,51 @@
+var height = 5;
+
+var width = 7;
+
+var area = width * height;
+
+console.log('area', area, typeof area);
+
+var bill = 100;
+
+var payment = 200;
+
+var change = payment - bill;
+
+console.log('change', change, typeof change);
+
+var quizzes = 20;
+
+var midterm = 80;
+
+var final = 200;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('grade', grade, typeof grade);
+
+var firstName = 'Matt';
+
+var lastName = 'Powers';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log('fullname', fullName, typeof fullName);
+
+var pH = 8;
+
+var isAcidic = pH < 7;
+
+console.log('is acidic', isAcidic, typeof isAcidic);
+
+var headCount = 23;
+
+var isSparta = headCount === 300;
+
+console.log('is sparta', isSparta, typeof isSparta);
+
+var motto = fullName;
+
+motto = motto + ' is the GOAT';
+
+console.log('motto', motto, typeof motto);
