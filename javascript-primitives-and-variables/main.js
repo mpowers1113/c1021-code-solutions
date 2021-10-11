ca89e9d2224051ad61cf0b35eb73649d41e1bf5a
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-11 10:45:10 -0700

    Completed javascript primitives and variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Matt Powers';
+
+var isCool = true;
+
+var totalPets = 0;
+
+var unknown = null;
+
+var never;
+
+console.log('never', never, typeof never);
+
+console.log('full name', fullName, typeof fullName);
+
+console.log('is cool', isCool, typeof isCool);
+
+console.log('total pets', totalPets, typeof totalPets);
+
+console.log('unknown', unknown, typeof unknown);
