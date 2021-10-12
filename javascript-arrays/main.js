0e302facd1bf705f3c06270ae70b116624a991e0
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-11 17:52:09 -0700

    Completed javascript-arrays exercise

---
+++ javascript-arrays/main.js
@@ -0,0 +1,27 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('value of colors[0]', colors[0]);
+console.log('value of colors[1]', colors[1]);
+console.log('value of colors[2]', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+console.log('colors array', colors);
+
+var students = ['Nietzsche', 'Kant', 'Carlyle', 'Mishima'];
+
+var numberOfStudents = students.length;
+
+console.log('number of students', numberOfStudents);
+
+var lastIndex = numberOfStudents - 1;
+
+var lastStudent = students[lastIndex];
+
+console.log('The last student is ' + lastStudent);
+
+console.log('value of students array', students);
