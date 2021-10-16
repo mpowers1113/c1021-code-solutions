530d1a4eb2e0720a4d66db62a103095ff86ab068
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-15 21:29:04 -0700

    Revised unneccessary for-in loop.

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -7,9 +7,7 @@
 function getStudentNames(students) {
   var newArr = [];
   for (var i = 0; i < students.length; i++) {
-    for (var name in students[i]) {
-      newArr.push(students[i][name]);
-    }
+    newArr.push(students[i].name);
   }
   return newArr;
 }
