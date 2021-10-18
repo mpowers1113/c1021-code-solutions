273fa04a9cf359cc188c4024820574d8893af221
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-18 16:47:24 -0700

    Completed DOM Manipulation exercise.

---
+++ dom-manipulation/main.js
@@ -0,0 +1,23 @@
+var $hotButton = document.querySelector('.hot-button');
+var $clickCount = document.querySelector('.click-count');
+
+$hotButton.addEventListener('click', incrementCounter);
+
+var numClicks = 1;
+
+function incrementCounter(event) {
+  $clickCount.textContent = 'Clicks: ' + numClicks++;
+  if (numClicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (numClicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (numClicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (numClicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (numClicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
