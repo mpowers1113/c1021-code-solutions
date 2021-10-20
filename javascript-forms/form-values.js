ea9fee0b2f5d096e49210aa6621bce9e60ef6105
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-20 11:27:28 -0700

    Comleted Javascript Forms Exercise.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,14 @@
+var $inputs = document.getElementById('contact-form').elements;
+var $form = document.getElementById('contact-form');
+
+$form.addEventListener('submit', getFormValues);
+
+function getFormValues(event) {
+  event.preventDefault();
+  var userInput = {
+    name: $inputs.name.value,
+    email: $inputs.email.value,
+    message: $inputs.message.value
+  };
+  console.log('userInput', userInput);
+}
