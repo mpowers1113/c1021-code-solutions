e2e9a5a12d1fce22c348cfae4beac223f425dc23
Matt Powers <powers.matthew.douglas@gmail.com>
2021-11-01 17:38:48 -0700

    Completed javascript constructors exercise.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,35 @@
+var coolObject = new Object();
+
+console.log('value of cool object', coolObject);
+
+console.log('typeof cool object ', typeof coolObject);
+
+var coolArray = new Array();
+
+console.log('value of coolArray', coolArray);
+
+console.log('typeof coolArray ', typeof coolArray);
+
+var coolFunction = new Function();
+
+console.log('value of coolFunction', coolFunction);
+
+console.log('typeof coolFunction ', typeof coolFunction);
+
+var coolString = new String();
+
+console.log('value of coolstring', coolString);
+
+console.log('typeof coolString ', typeof coolString);
+
+var coolNumber = new Number();
+
+console.log('value of coolNumber', coolNumber);
+
+console.log('typeof coolNumber ', typeof coolNumber);
+
+var coolBoolean = new Boolean();
+
+console.log('value of coolBoolean', coolBoolean);
+
+console.log('typeof coolBoolean ', typeof coolBoolean);
