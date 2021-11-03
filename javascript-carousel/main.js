2fe9061d818830e2b2555031e2ce9228203610e4
Matt Powers <powers.matthew.douglas@gmail.com>
2021-11-03 16:49:42 -0700

    Revised with clickable dots & separate arrow click handlers.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -9,16 +9,10 @@ var imageArray = [
   './images/001.png', './images/004.png', './images/007.png', './images/025.png', './images/039.png'
 ];
 
-function changeImageHandler(event) {
-  var whichArrow = event.target.getAttribute('data-view') || 'right';
-  if (whichArrow === 'left' && currentImageArrayIndex === 0) {
-    currentImageArrayIndex = 4;
-  } else if (whichArrow === 'left' && currentImageArrayIndex > 0) {
-    currentImageArrayIndex--;
-  }
-  if (whichArrow === 'right' && currentImageArrayIndex === 4) {
+function changeImageHandlerRight(event) {
+  if (currentImageArrayIndex === 4) {
     currentImageArrayIndex = 0;
-  } else if (whichArrow === 'right' && currentImageArrayIndex < 4) {
+  } else {
     currentImageArrayIndex++;
   }
   $image.setAttribute('style', `background-image: url(${imageArray[currentImageArrayIndex]})`);
@@ -33,8 +27,26 @@ function changeImageHandler(event) {
   myCarousel = setInterval(carouselHandler, 2000);
 }
 
-$leftArrow.addEventListener('click', changeImageHandler);
-$rightArrow.addEventListener('click', changeImageHandler);
+function changeImageHandlerLeft(event) {
+  if (currentImageArrayIndex === 0) {
+    currentImageArrayIndex = 4;
+  } else {
+    currentImageArrayIndex--;
+  }
+  $image.setAttribute('style', `background-image: url(${imageArray[currentImageArrayIndex]})`);
+  for (var i = 0; i < $dots.length; i++) {
+    if (i === currentImageArrayIndex) {
+      $dots[i].setAttribute('class', 'fas fa-circle');
+    } else {
+      $dots[i].setAttribute('class', 'far fa-circle');
+    }
+  }
+  clearInterval(myCarousel);
+  myCarousel = setInterval(carouselHandler, 2000);
+}
+
+$leftArrow.addEventListener('click', changeImageHandlerLeft);
+$rightArrow.addEventListener('click', changeImageHandlerRight);
 
 function carouselHandler() {
   if (currentImageArrayIndex === 4) {
@@ -53,3 +65,19 @@ function carouselHandler() {
 }
 
 var myCarousel = setInterval(carouselHandler, 2000);
+
+function dotsImageChangeHandler(event) {
+  var dataDotVal = event.target.getAttribute('data-dot');
+  $image.setAttribute('style', `background-image: url(${imageArray[Number(dataDotVal)]})`);
+  for (var i = 0; i < $dots.length; i++) {
+    if (i === Number(dataDotVal)) {
+      $dots[i].setAttribute('class', 'fas fa-circle');
+    } else {
+      $dots[i].setAttribute('class', 'far fa-circle');
+    }
+  }
+  clearInterval(myCarousel);
+  myCarousel = setInterval(carouselHandler, 2000);
+}
+
+$dots.forEach(dot => dot.addEventListener('click', dotsImageChangeHandler));
