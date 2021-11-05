c96655667317d158c7039e95d7654bde47166c3b
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-11-05 19:59:18 +0000

    Completed Javascript Ajax Exercise.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,24 @@
+var $users = document.querySelector('#user-list');
+
+function userRequestHandler() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    var users = xhr.response;
+    if (xhr.status === 200) {
+
+      for (var i = 0; i < users.length; i++) {
+        var eachUser = users[i];
+        var $li = document.createElement('li');
+        $li.textContent = eachUser.name;
+        $users.appendChild($li);
+      }
+    }
+  });
+  xhr.send();
+}
+
+userRequestHandler();
