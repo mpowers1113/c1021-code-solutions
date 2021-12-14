be8afdd43f97fc2d922b73291f17459e4f067e07
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-14 22:57:17 +0000

    Refactored to accept data via props.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,41 +3,40 @@ import React from 'react';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { HTML: false, CSS: false, JS: false };
-    this.toggleHTML = this.toggleHTML.bind(this);
-    this.toggleCSS = this.toggleCSS.bind(this);
-    this.toggleJS = this.toggleJS.bind(this);
+    this.state = { titles: [...this.props.titles], descriptions: [...this.props.descriptions], visibleDescription: null };
+    this.toggle = this.toggle.bind(this);
   }
 
-  toggleHTML() {
-    this.setState({ HTML: true, CSS: false, JS: false });
-  }
+  // componentDidMount() {
+  //   const titles = this.props.titles;
+  //   const descriptions = this.props.descriptions;
+  //   for (let i = 0; i < titles.length; i++) {
+  //     this.setState({ titles: [...this.titles, titles[i]], descriptions: [...this.descriptions, descriptions[i]], isVisible: [...this.isVisible, false] });
+  //   }
+  // }
 
-  toggleCSS() {
-    this.setState({ HTML: false, CSS: true, JS: false });
-  }
-
-  toggleJS() {
-    this.setState({ HTML: false, CSS: false, JS: true });
+  toggle(event) {
+    const targetIndex = event.target.id;
+    for (let i = 0; i < this.state.titles.length; i++) {
+      if (targetIndex === this.state.titles[i]) {
+        this.setState({ visibleDescription: i });
+      }
+    }
   }
 
   render() {
+
     return (
-      <div>
-        <div onClick={this.toggleHTML} className="column-50">
-          <h1 onClick={this.toggleHTML} >HTML</h1>
-          {this.state.HTML && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum laboriosam, voluptates excepturi voluptas labore laudantium atque consequatur voluptate ut earum, harum quia quo numquam voluptatibus ad aliquam tempore itaque.</p>}
-        </div>
-        <div onClick={this.toggleCSS} className="column-50" >
-          <h1 onClick={this.toggleCSS} >CSS</h1>
-          {this.state.CSS && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum laboriosam, voluptates excepturi voluptas labore laudantium atque consequatur voluptate ut earum, harum quia quo numquam voluptatibus ad aliquam tempore itaque.</p>}
-         </div>
-          <div onClick={this.toggleJS} className="column-50">
-            <h1 onClick={this.toggleJS} >JavaScript</h1>
-            {this.state.JS && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum laboriosam, voluptates excepturi voluptas labore laudantium atque consequatur voluptate ut earum, harum quia quo numquam voluptatibus ad aliquam tempore itaque.</p>}
+        <div>
+          {this.state.titles.map((title, index) =>
+          <div onClick={this.toggle} key = {index.toString()} className="column-50">
+            <h1 onClick={this.toggle} id={title}>{title}</h1>
+            <p className={index === this.state.visibleDescription ? '' : 'hidden'}>{this.state.descriptions[index]}</p>
           </div>
-      </div>
+          )}
+        </div>
     );
+
   }
 }
 
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
 const App = props => {
+
   return (
       <div>
-        <Accordion/>
+        <Accordion descriptions={['Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum laboriosam, voluptates excepturi voluptas labore laudantium atque consequatur voluptate ut earum, harum quia quo numquam voluptatibus ad aliquam tempore itaque.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum laboriosam, voluptates excepturi voluptas labore laudantium atque consequatur voluptate ut earum, harum quia quo numquam voluptatibus ad aliquam tempore itaque.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum laboriosam, voluptates excepturi voluptas labore laudantium atque consequatur voluptate ut earum, harum quia quo numquam voluptatibus ad aliquam tempore itaque.']} titles={['HTML', 'CSS', 'JS']}/>
       </div>
   );
 };
