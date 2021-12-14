5b2cc7f55675c95ace6ed44b8525a7d4e40a4e2f
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-14 23:55:30 +0000

    Refactored without adding images to state.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,30 +4,16 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentIndex: 0,
-      image: []
+      currentIndex: 0
     };
     this.toggleRight = this.toggleRight.bind(this);
     this.toggleLeft = this.toggleLeft.bind(this);
     this.dotToggle = this.dotToggle.bind(this);
-    this.addImage = this.addImage.bind(this);
-    this.addImage();
-  }
-
-  addImage() {
-    const imageArr = this.props.image;
-    for (const img of imageArr) {
-      if (this.state.image.includes(img) === false) {
-        const newImage = { url: img, key: this.state.image.length + 1 };
-        this.state.image.push(newImage);
-      }
-    }
-    this.props.image.length = 0;
   }
 
   toggleLeft() {
     if (this.state.currentIndex === 0) {
-      this.setState({ currentIndex: this.state.image.length - 1 });
+      this.setState({ currentIndex: this.props.image.length - 1 });
     } else {
       const minusOne = this.state.currentIndex - 1;
       this.setState({ currentIndex: minusOne });
@@ -35,7 +21,7 @@ class Carousel extends React.Component {
   }
 
   toggleRight() {
-    if (this.state.currentIndex === this.state.image.length - 1) {
+    if (this.state.currentIndex === this.props.image.length - 1) {
       this.setState({ currentIndex: 0 });
     } else {
       const plusOne = this.state.currentIndex + 1;
@@ -51,15 +37,16 @@ class Carousel extends React.Component {
   render() {
     return (
       <div className="container">
-        <div key={this.state.image[this.state.currentIndex].key} className="row card">
-          <div className="column-20 left"><i onClick={this.toggleLeft} className="fas fa-chevron-left fa-3x"></i></div>
-          <div className="column-80"><img src={this.state.image[this.state.currentIndex].url} alt="image" /></div>
-          <div className="column-20 right"><i onClick={this.toggleRight} className="fas fa-chevron-right fa-3x"></i></div></div>
-        <div className="row dots">{this.state.image.map((img, index) => {
-          return <i key= {index} id={index} onClick={this.dotToggle} className={index === this.state.currentIndex ? 'fas fa-circle' : 'far fa-circle'}></i>;
-
-        })}
-        </div>
+          {this.props.image.map((image, index) =>
+          <div key={index} className={this.state.currentIndex === index ? 'row card' : 'hidden'}>
+            <div className="column-20 left"><i onClick={this.toggleLeft} className="fas fa-chevron-left fa-3x"></i></div>
+            <div className="column-80"><img src={image} alt={'image' + index} /></div>
+            <div className="column-20 right"><i onClick={this.toggleRight} className="fas fa-chevron-right fa-3x"></i></div>
+          </div>)}
+          <div className="row dots">
+          {this.props.image.map((image, index) =>
+            <i id={index} key={index}onClick={this.dotToggle} className={index === this.state.currentIndex ? 'fas fa-circle' : 'far fa-circle'}></i>
+          )}</div>
       </div>
     );
   }
