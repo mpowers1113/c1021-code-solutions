36e6cd5181485da8b5371c45800a0fd805f01573
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-16 03:02:15 +0000

    Completed fetch-in-react-practice

---
+++ fetch-in-react-practice/client/components/app.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import PageTitle from './page-title';
+import TodoList from './todo-list';
+import TodoForm from './todo-form';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/todos')
+      .then(res => {
+        if (!res.ok) throw new Error('Response is not ok');
+        else return res.json();
+      })
+      .then(res => this.setState({ todos: res }))
+      .catch(err => console.error(err));
+  }
+
+  addTodo(newTodo) {
+
+    fetch('http://localhost:3000/api/todos', {
+      method: 'POST',
+      headers: {
+        Accept: 'aplication/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTodo)
+    })
+      .then(res => {
+        if (!res.ok) throw new Error('error in post request');
+        else return res.json();
+      })
+      .then(res => {
+        this.setState({ todos: this.state.todos.concat(res) });
+      });
+  }
+
+  toggleCompleted(todoId) {
+    fetch(`http://localhost:3000/api/todos/${todoId}`, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ isCompleted: true })
+    })
+      .then(res => res.json())
+      .then(res => {
+        const arrayCopy = this.state.todos;
+        arrayCopy[todoId - 1] = res;
+        this.setState({ todos: arrayCopy });
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col pt-5">
+            <PageTitle text="Todo App"/>
+            <TodoForm onSubmit={this.addTodo}/>
+            <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
