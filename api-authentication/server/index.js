4c167b407d7d29a363df4030c1e7c918fc07815d
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-17 18:11:02 +0000

    Completed api-authentication exercise.

---
+++ api-authentication/server/index.js
@@ -0,0 +1,98 @@
+require('dotenv/config');
+const pg = require('pg');
+const argon2 = require('argon2'); // eslint-disable-line
+const express = require('express');
+const jwt = require('jsonwebtoken'); // eslint-disable-line
+const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
+
+const db = new pg.Pool({ // eslint-disable-line
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const app = express();
+
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+        insert into "users" ("username", "hashedPassword")
+        values ($1, $2)
+        returning "userId", "username", "createdAt"
+      `;
+      const params = [username, hashedPassword];
+      return db.query(sql, params);
+    })
+    .then(result => {
+      const [user] = result.rows;
+      res.status(201).json(user);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  argon2.hash(password).then(hashedPassword => {
+    const sql = `
+          insert into "users" ("hashedPassword", "username")
+          values ($1, $2)
+          returning "userId", "createdAt", "username"
+          `;
+    const params = [hashedPassword, username];
+    db.query(sql, params)
+      .then(result => {
+        const [newUser] = result.rows;
+        res.status(201).json(newUser);
+      }).catch(err => {
+        console.error(err);
+        throw new ClientError(500, 'an unexpected error ocurred');
+      });
+  });
+});
+
+app.post('/api/auth/sign-in', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(401, 'invalid login');
+  }
+  const sql = `
+              select "hashedPassword", "userId"
+              from "users"
+              where "username" = $1`;
+  const params = [username];
+  db.query(sql, params)
+    .then(result => {
+      const [userCheck] = result.rows;
+      if (!userCheck) throw new ClientError(401, 'invalid username');
+      const { userId, hashedPassword } = userCheck;
+      return argon2.verify(hashedPassword, password)
+        .then(matching => {
+          if (!matching) throw new ClientError(401, 'invalid login');
+          const payload = { userId, username };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.status(200).json({ token, user: payload });
+        }).catch(err => next(err));
+    }).catch(err => next(err));
+});
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`express server listening on port ${process.env.PORT}`);
+});
