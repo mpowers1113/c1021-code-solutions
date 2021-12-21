54f4d4052c3f6b4fdb78a49f237b70a934fa14a4
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-21 19:42:18 +0000

    Added counter function.

--- javascript-closures/main.js
+++ javascript-closures/main.js
@@ -13,3 +13,31 @@ const readingDryDocumentsAllDay = lawSchool('Ryan Florence');
 const me = softwareEngineer('Matt Powers');
 
 console.log(whatMyParentsHopedIdBe, '\n', readingDryDocumentsAllDay, '\n', me);
+
+var makeCounter = function () {
+  var privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment: function () {
+      changeBy(1);
+    },
+
+    decrement: function () {
+      changeBy(-1);
+    },
+
+    value: function () {
+      return privateCounter;
+    }
+  };
+};
+
+const counterOne = makeCounter();
+const counterTwo = makeCounter();
+
+counterOne.increment();
+counterTwo.decrement();
+
+console.log('counterOne value', counterOne.value(), 'counterTwo value', counterTwo.value());
