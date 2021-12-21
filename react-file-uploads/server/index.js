e270e271ee41f7233eaba5ee4157979c1c91eff7
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-21 01:41:49 +0000

    Completed react file uploads exercise.

---
+++ react-file-uploads/server/index.js
@@ -0,0 +1,55 @@
+require('dotenv/config');
+const pg = require('pg');
+const express = require('express');
+const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
+const uploadsMiddleware = require('./uploads-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const app = express();
+
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.post('/api/uploads', uploadsMiddleware, (req, res, next) => {
+  const { caption } = req.body;
+  if (!caption) {
+    throw new ClientError(400, 'caption is a required field');
+  }
+  const filePath = '/images' + req.file.path;
+  const sql = `insert into "images" ("caption", "url")
+               values ($1, $2)
+               returning * `;
+  const params = [caption, filePath];
+  db.query(sql, params)
+    .then(res => {
+      const [image] = res.rows;
+      console.log(image);
+    }).catch(err => next(err));
+});
+
+app.get('/api/images', (req, res, next) => {
+  const sql = `
+    select *
+      from "images"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`express server listening on port ${process.env.PORT}`);
+});
